04dcb55f07bb843ca70d1ffe77f021b867df3423
Anar_Osmanov <anar_osmanov@vk.com>
2025-06-05 23:44:19 +0400

    Sound bag fix

--- script/script.js
+++ script/script.js
@@ -15,10 +15,10 @@ const thirdScene = document.querySelector(".third-scene");
 const endScene = document.querySelector(".the-end");
 
 //--------------------------------------- Аудио файлы -------------------------------------------------------------
-const powerLossSound = new Audio("/sounds/потеря-мощности.mp3");
-const crashLoadingSound = new Audio("/sounds/аварийная-посадка.mp3");
-const takeoffSound = new Audio("/sounds/Взлет-космического-корабля.mp3");
-const backgroundSound = new Audio("/sounds/фон.mp3");
+const powerLossSound = new Audio("../sounds/powerLoss.mp3");
+const crashLoadingSound = new Audio("../sounds/crashLanding.mp3");
+const takeoffSound = new Audio("../sounds/shipTakeoff.mp3.mp3");
+const backgroundSound = new Audio("../sounds/backgroundSound.mp3");
 //--------------------------------------- механики игры ----------------------------------------------------------
 let currentQuestion = 0;
 let score = 0;
